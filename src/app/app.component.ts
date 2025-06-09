c878e01c113af9edbc847020a6f4c6d6361b61ab
lokesh <lokeshpaduchuri@gmail.com>
2025-06-09 15:06:58 -0500

    Updates to mobile navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, Scroll } from '@angular/router';
 import { trigger, transition, style, animate, query, group } from '@angular/animations';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { ViewportScroller } from '@angular/common';
+import { filter } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -10,9 +13,9 @@ import { FooterComponent } from "./components/footer/footer.component";
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
   template: `
     <app-navbar></app-navbar>
-    <main class="pt-16 min-h-[calc(100vh-4rem)]" [@routeAnimations]="prepareRoute(outlet)">
-      <router-outlet #outlet="outlet"></router-outlet>
-    </main>
+    <main class="pt-10 min-h-[calc(100vh-4rem)] text-white" [@routeAnimations]="prepareRoute(outlet)">
+  <router-outlet #outlet="outlet"></router-outlet>
+</main>
     <app-footer></app-footer>
   `,
   animations: [
@@ -44,6 +47,19 @@ import { FooterComponent } from "./components/footer/footer.component";
 ],
 })
 export class AppComponent {
+
+  constructor(private router: Router, private viewportScroller: ViewportScroller) {
+    this.router.events
+      .pipe(filter((e): e is Scroll => e instanceof Scroll))
+      .subscribe((e) => {
+        // Scroll to top on forward navigation
+        if (e.position === null) {
+          this.viewportScroller.scrollToPosition([0, 0]);
+          this.viewportScroller.scrollToPosition([0, 64]); 
+        }
+      });
+  }
+  
   prepareRoute(outlet: RouterOutlet) {
     return outlet?.activatedRouteData?.['animation'] || null;
   }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styles: [`
-    nav { background-color: #000; height: 60px; /* fixed height */
-  line-height: 60px; /* vertically center text */
+    nav { background-color: #000;  /* fixed height */
+      min-height: 64px; /* fixed height */
+      line-height: 60px; /* vertically center text */
   overflow: hidden;}
     a { color: white; }
     a:hover { text-decoration: underline; }
   `],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+}
