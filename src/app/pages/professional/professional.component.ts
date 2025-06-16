45eedb58ecc6fa55f0ca650700c99fd8243f1c31
Lokesh Paduchuri <lokeshpaduchuri@gmail.com>
2025-06-16 15:48:44 -0500

    Add SEO title and meta descriptions

--- src/app/pages/professional/professional.component.ts
+++ src/app/pages/professional/professional.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 import { Project, PROJECTS } from '../../data/projects';
 import { CommonModule } from '@angular/common'; // Import CommonModule for Angular directives
 import { ProjectModalComponent } from '../../components/project-modal/project-modal.component';
@@ -10,9 +11,16 @@ import { ProjectModalComponent } from '../../components/project-modal/project-mo
   templateUrl: './professional.component.html',
   styleUrls: ['./professional.component.scss']
 })
-export class ProfessionalComponent {
+export class ProfessionalComponent implements OnInit {
   projects: Project[] = PROJECTS;
   selectedProject: Project | null = null;
+
+  constructor(private titleService: Title, private meta: Meta) {}
+
+  ngOnInit() {
+    this.titleService.setTitle('Professional Work - Lowkeyframes');
+    this.meta.updateTag({ name: 'description', content: 'Browse professional projects and collaborations.' });
+  }
 
   openProjectModal(project: Project) {
     this.selectedProject = project;
